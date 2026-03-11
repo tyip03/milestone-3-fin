28ed664769ad6a454f58b425b6c4ddc44ba27a4d
tyip03 <terrancehaysyip@gmail.com>
2026-03-11 02:23:29 -0700

    Update transaction.py

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -10,7 +10,7 @@ class Transaction:
     def __init__(self):
         self.queries = []
         self.undo_log = []
-        self.tid = id(self)
+        self._rolling_back = False
 
 
     """
@@ -25,100 +25,88 @@ class Transaction:
         # use grades_table for aborting
 
         
+    def _build_undo_entry(self, query, table, args):
+        """
+        Build inverse operation BEFORE the query runs.
+        Returns:
+            None if no undo needed
+            tuple(callable, args) for rollback
+        """
+        query_name = query.__name__
+        qobj = query.__self__   # the Query instance bound to this method
+
+        # INSERT -> undo is DELETE(primary_key)
+        if query_name == "insert":
+            key = args[table.key]
+            return (qobj.delete, (key,))
+
+        # UPDATE -> undo is UPDATE(primary_key, old_values_for_changed_columns)
+        elif query_name == "update":
+            primary_key = args[0]
+            new_columns = args[1:]
+
+            old_records = qobj.select(primary_key, table.key, [1] * table.num_columns)
+            if not old_records:
+                return None
+
+            old_record = old_records[0].columns
+            rollback_columns = [None] * table.num_columns
+
+            for i in range(table.num_columns):
+                if i == table.key:
+                    rollback_columns[i] = None
+                elif new_columns[i] is not None:
+                    rollback_columns[i] = old_record[i]
+
+            return (qobj.update, (primary_key, *rollback_columns))
+
+        # DELETE -> undo is INSERT(old_full_row)
+        elif query_name == "delete":
+            primary_key = args[0]
+
+            old_records = qobj.select(primary_key, table.key, [1] * table.num_columns)
+            if not old_records:
+                return None
+
+            old_record = old_records[0].columns
+            return (qobj.insert, tuple(old_record))
+
+        # SELECT / SUM / other reads do not need undo
+        return None
+
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
     def run(self):
+        self.undo_log = []
+        self._rolling_back = False
+
         for query, table, args in self.queries:
-            query_name = query.__name__
-            primary_key = args[0] if len(args) > 0 else None
-            table_query = Query(table)
-            
-            lock_id = primary_key
-            if query_name in ["select", "update", "delete"]:
-                rids = table.index.locate(table.key, primary_key)
-                if not rids:
-                    return self.abort()
-                lock_id = rids[0]
-            
-            if query_name == "select":
-                if not table.lock_manager.get_s_lock(self.tid, lock_id):
-                    return self.abort()
-            elif query_name in ["update", "delete", "insert"]:
-                if not table.lock_manager.get_x_lock(self.tid, lock_id):
-                    return self.abort()
-                
-            if query_name == "update":
-                old_record = table_query.select(primary_key, table.key, [1] * table.num_columns)
-                if old_record == False or len(old_record) == 0:
-                    return self.abort()
-                
-                old_vals = old_record[0].columns.copy()
-                self.undo_log.append(("update", table, primary_key, old_vals))
-                
-            elif query_name == "delete":
-                old_record = table_query.select(primary_key, table.key, [1] * table.num_columns)
-                if old_record == False or len(old_record) == 0:
-                    return self.abort()
-                
-                old_vals = old_record[0].columns.copy()
-                self.undo_log.append(("delete", table, old_vals))
-                
-            elif query_name == "insert":
-                existing = table_query.select(primary_key, table.key, [1] * table.num_columns)
-                if existing and len(existing) > 0:
-                    return self.abort()
-                self.undo_log.append(("insert", table, primary_key))
-                
+            undo_entry = self._build_undo_entry(query, table, args)
+
             result = query(*args)
+
+            # If the query has failed the transaction should abort
             if result == False:
                 return self.abort()
+
+            if undo_entry is not None:
+                self.undo_log.append(undo_entry)
+
         return self.commit()
 
-    
     def abort(self):
-        #TODO: do roll-back and any other necessary operations
-        
-        for entry in reversed(self.undo_log):
-            action = entry[0]
-            
-            if action == "update":
-                _, table, primary_key, old_vals = entry
-                
-                restored_cols = old_vals.copy()
-                restored_cols[table.key] = None
-                table_query = Query(table)
-                table_query.update(primary_key, *restored_cols)
-            
-            elif action == "delete":
-                _, table, old_vals = entry
-                
-                table_query = Query(table)
-                table_query.insert(*old_vals)
-            
-            elif action == "insert":
-                _, table, primary_key = entry
-                
-                table_query = Query(table)
-                table_query.delete(primary_key)
+        # roll back in reverse order
+        self._rolling_back = True
 
-        released = set()
-        for _, table, _ in self.queries:
-            if id(table) not in released:
-                table.lock_manager.release_locks(self.tid)
-                released.add(id(table))
-        
-        self.undo_log.clear()
+        for undo_query, undo_args in reversed(self.undo_log):
+            try:
+                undo_query(*undo_args)
+            except Exception:
+                pass
+
+        self.undo_log = []
+        self._rolling_back = False
         return False
 
-    
     def commit(self):
-        # TODO: commit to database
-        
-        released = set()
-        for _, table, _ in self.queries:
-            if id(table) not in released:
-                table.lock_manager.release_locks(self.tid)
-                released.add(id(table))
-        
-        self.undo_log.clear()
-        
+        self.undo_log = []
         return True
